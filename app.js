848d3dd20e870b2754be24ccd4e474f941484961
David Chaban <davidchaban01@gmail.com>
2022-11-12 17:55:56 -0300

    add dependencies

--- app.js
+++ app.js
@@ -5,28 +5,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./config/database')
+// const errorHandler = require('./middlewares/errorHandler')
+
 var indexRouter = require('./routes/index');
 
-
+const cors = require('cors');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+const middlewareTime = (req,res, next) => {
+  console.log('Time:', Date.now()) 
+  // Imprime por consola cada vez q sale una peticion
+  next()
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(middlewareTime)
+// app.use(errorHandler.notFound)
+app.use(cors());
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 let router = require('express').Router()
 
-
 let userRoute = require('./userRoute')
 
+let cityRoute = require('./cityRoute')
 
-
-router.use('/api', userRoute)
+const middlewareTime = (req,res, next) => {
+    console.log('Time:', Date.now()) // Imprime por consola cada vez q sale una peticion
+    next()
+  }
+const bodyUser = (req, res, next) => {
+    if(req.body.name.length > 3) {
+    next()
+}
+    else {
+         throw new Error('El nombre es muy corto')
+    }
+}
+// router.use('/user', middlewareTime, bodyUser, userRoute)
+// router.use('/api/users', userRoute)
+router.use('/cities', middlewareTime, cityRoute)
+router.use('/user', userRoute)
 
 
 module.exports = router;
