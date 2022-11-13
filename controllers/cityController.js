da9c24734add1b69f5e0e5f417d28ad9a2892a04
David Chaban <davidchaban01@gmail.com>
2022-11-13 15:51:51 -0300

    added route update

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -17,6 +17,51 @@ const cityCreated = {
             })
         }
     },
-} 
+
+    update: async(req,res) => {
+    let {id} = req.params
+
+    try {
+      let cityUpdate = await City.findOneAndUpdate({_id: id}, req.body, {new: true})
+      if(cityUpdate){
+        res.status(200).json({
+          id: cityUpdate,
+          success: true,
+          message: "successfully update"  
+        })
+      }else{
+        res.status(404).json({
+          success:false,
+          message: "City no Found"
+        })
+      }
+
+    }catch(error){
+      res.status(400).json({
+        success: false,
+        message: error.message
+
+    }
+
+
+
+   )}
+  },
+  
+}
+
+
+
+
 
 module.exports = cityCreated
+
+
+// Como admin de la app
+// Quiero editar una ciudad
+// Para actualizar los datos de la ciudad
+
+// Crear ruta/controlador para editar una ciudad
+//     método: put
+//     ruta: /api/cities/:id
+//     realizar pruebas con postman
