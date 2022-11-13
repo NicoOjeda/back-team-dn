b81fe7312885d8339ad70c63982697809fd4d848
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-13 20:50:13 -0300

    se ruta de update para actualizar show

--- routes/show.js
+++ routes/show.js
@@ -1,8 +1,9 @@
 let router = require('express').Router()
 
-let { create } = require('../controllers/show')
+let { create , update } = require('../controllers/show')
 
 
 router.post('/', create)
+router.patch('/:id', update)
 
 module.exports = router
