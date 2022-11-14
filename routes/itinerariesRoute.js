1d9ba0167b623da7ec6eedde7fbacd2b019ff58b
David Chaban <davidchaban01@gmail.com>
2022-11-14 17:44:52 -0300

    Added route for itinerary

--- routes/itinerariesRoute.js
+++ routes/itinerariesRoute.js
@@ -1,10 +1,12 @@
 let router = require('express').Router();
-const { create, read , update, destroy  } = require('../controllers/itineraryController')
+const { create, read , update, destroy, readOne  } = require('../controllers/itineraryController')
 
 
 router.post('/', create );
-router.route('/read').get(read);
+router.get('/',read);
 router.put('/:id',update);
 router.delete('/:id', destroy)
-  
+// router.get('/:id', readOne) 
+
+// router.get('/?cityId=id',readOne);
 module.exports = router;
