eac245fd190d6cb598eb76dd23ffc0d1d6acb494
David Chaban <davidchaban01@gmail.com>
2022-11-14 12:01:40 -0300

    index fix

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,25 @@
-let router = require('express').Router()
+let router = require("express").Router();
+let hotel = require("./hotel");
+let show = require("./show");
+let userRoute = require("./userRoute");
+let itinerariesRoute = require("./itinerariesRoute");
+let cityRoute = require("./cityRoute");
 
-
-
-let hotel = require('./hotel')
-let show = require('./show')
-
-
-
-
-router.use('/hotels', hotel)
-router.use('/shows', show)
-
-
-
-
-
-
-let userRoute = require('./userRoute')
-
-let cityRoute = require('./cityRoute')
-const cityCreated = require('../controllers/cityController')
-let itinerariesRoute = require('./itinerariesRoute')
-
-const middlewareTime = (req,res, next) => {
-    console.log('Time:', Date.now()) // Imprime por consola cada vez q sale una peticion
-    next()
-  }
+const middlewareTime = (req, res, next) => {
+  console.log("Time:", Date.now()); // Imprime por consola cada vez q sale una peticion
+  next();
+};
 const bodyUser = (req, res, next) => {
-    if(req.body.name.length > 3) {
-    next()
-}
-    else {
-         throw new Error('El nombre es muy corto')
-    }
-}
-router.use('/user', middlewareTime, bodyUser, userRoute)
-router.use('/api/users', userRoute)
-router.use('/cities', middlewareTime, cityRoute)
-router.use('/user', userRoute)
-router.use('/itineraries', itinerariesRoute)
-// router.use('/id')
-// router.use('/City',cityCreated)
-
-
-
+  if (req.body.name.length > 3) {
+    next();
+  } else {
+    throw new Error("El nombre es muy corto");
+  }
+};
+router.use("/user", middlewareTime, bodyUser, userRoute);
+router.use("/cities", middlewareTime, cityRoute);
+router.use("/itineraries", itinerariesRoute);
+router.use("/hotels", hotel);
+router.use("/shows", show);
 
 module.exports = router;
-  
