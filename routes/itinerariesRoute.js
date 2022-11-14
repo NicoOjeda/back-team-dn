b08510034ce3581cd2848193ca0640cf5b63576f
David Chaban <davidchaban01@gmail.com>
2022-11-14 00:15:37 -0300

    Added Routes for itineraries

--- routes/itinerariesRoute.js
+++ routes/itinerariesRoute.js
@@ -1,11 +1,10 @@
 let router = require('express').Router();
-const { create, read , update } = require('../controllers/itineraryController')
+const { create, read , update, destroy  } = require('../controllers/itineraryController')
 
 
 router.post('/', create );
 router.route('/read').get(read);
 router.put('/:id',update);
-// router.put('/:id', update);
-
+router.delete('/:id', destroy)
   
 module.exports = router;
