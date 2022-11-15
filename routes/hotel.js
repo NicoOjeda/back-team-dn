d95f2fb9b20867be9970f5f1f2d56658bcee3310
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-14 23:40:16 -0300

    se crea ruta para ver datos de hotel

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,9 +1,10 @@
 let router = require('express').Router()
 
-let {create, read, update, destroy} = require('../controllers/hotel') 
+let {create, read, update, destroy, all} = require('../controllers/hotel') 
 
 router.post('/', create) 
-router.get('/', read) 
+router.get('/', all)
+router.get('/:id', read)
 router.patch('/:id', update)
 router.delete('/:id', destroy)
 
