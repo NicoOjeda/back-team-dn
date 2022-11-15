b3b512462423c436edde94286e361b9967a67da1
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-15 05:03:15 -0300

    se modifica city

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -21,10 +21,9 @@ const cityCreated = {
   },
   read: async (req, res) => {
     let query = {}
-    let order = {}
     console.log(req.query)
     if(req.query.name){
-      query = {name:  {  $regex : req.query.name }}
+      query = {name:  {  $regex :  req.query.name, $options: 'i' + req.query.name  }}
     }
     if(req.query.continent){
       query = { 
