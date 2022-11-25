64dd66de5f02d5363feaf0df47c4bd88f77f9272
David Chaban <davidchaban01@gmail.com>
2022-11-25 12:34:41 -0300

    Se corrige ruta

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,16 @@
 
 
 const router = require('express').Router();
-const schema = require('../models/User')
 const validator = require('../middlewares/validator')
 const { signup , verified } = require('../controllers/userController')
 const accountExists = require("../middlewares/accountExistsSignUp");
 const { verifiedaccess } = require('googleapis/build/src/apis/verifiedaccess');
+const schema = require("../schemas/user")
 // const { accountExist, accountExists } = require('../middlewares/accountExistsSignUp')
 //primero valido con joi
 // luego verifico si la cuenta existe
 // y si todo va bien, creo el usuario
-router.post('/signup', signup)
+router.post('/signup',validator(schema), signup)
 router.get('/verify/:code',verified )
 
 // validator(schema),accountExists,
