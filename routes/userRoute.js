b1e1cb3d8a0582f399830bbe3291e0c6d76f183a
David Chaban <davidchaban01@gmail.com>
2022-11-27 21:05:37 -0300

    se agrega route signout

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const validator = require('../middlewares/validator');
-const { signup , verified, signin, logInToken, readUser, updateUser } = require('../controllers/userController');
+const { signup , verified, signin, logInToken, readUser, updateUser, signout } = require('../controllers/userController');
 // const accountExists = require("../middlewares/accountExistsSignUp");
 // const { verifiedaccess } = require('googleapis/build/src/apis/verifiedaccess');
 const schema = require("../schemas/user");
@@ -21,6 +21,7 @@ router.post('/signin',validator(signInSchema), accountExists, accountHasBeenVeri
 router.post('/token', passport.authenticate('jwt',{session:false}), mustSignIn, logInToken )
 router.get('/me/:id', readUser)
 router.patch('/me/:id', updateUser)
+router.put("/sign-out",passport.authenticate('jwt',{session:false}), signout )
 
 // validator(schema),accountExists,
 module.exports = router
