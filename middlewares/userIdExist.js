16428c4a846db24f2e2b32bdaa56ccfa49fe4bdc
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-30 11:40:38 -0300

    se midifican middlewares para eliminar hoteles

--- middlewares/userIdExist.js
+++ middlewares/userIdExist.js
@@ -1,10 +1,10 @@
-const idnotauthorized = require("../config/responses")
+const {idnotauthorized} = require("../config/responses")
 let Hotel = require('../models/Hotel')
 
 const hotelUIdExists =  async(req,res,next) => {
     let hotel = await Hotel.findOne({_id: req.params.id})
 
-    if(hotel.userId === req.user._id){
+    if(hotel?.userId.equals(req.user.id)){
       
         return next()
     } else{
