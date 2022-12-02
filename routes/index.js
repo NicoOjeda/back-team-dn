b57c4be66c10cd47c487556ff5c0e88ee3d055e9
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-01 21:07:15 -0300

    se agrega ruta comment

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ let hotel = require("./hotel");
 let show = require("./show");
 let itinerariesRoute = require("./itinerariesRoute");
 let cityRoute = require("./cityRoute");
+let comment = require('./comment')
 const userRoute = require('./userRoute')
 const middlewareTime = (req, res, next) => {
   console.log("Time:", Date.now()); // Imprime por consola cada vez q sale una peticion
@@ -20,5 +21,6 @@ router.use("/cities", cityRoute);
 router.use("/itineraries", itinerariesRoute);
 router.use("/hotels", hotel);
 router.use("/shows", show);
-router.use('/auth', userRoute)
+router.use('/auth', userRoute);
+router.use('/comments', comment);
 module.exports = router;
