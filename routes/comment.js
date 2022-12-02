5a66145386939169267f3693072b147e20ee08f8
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-02 09:02:29 -0300

    se define ruta comments

--- routes/comment.js
+++ routes/comment.js
@@ -4,11 +4,14 @@ const passport = require('../config/passport')
 const validator = require('../middlewares/validator')
 const schema = require('../schemas/comment')
 
-let { create, read } = require('../controllers/comment')
+let { create, read, update, destroy } = require('../controllers/comment')
+const CommentIdExists = require('../middlewares/userIdComment')
 
 
 router.get('/', read)
 router.post('/',validator(schema), passport.authenticate('jwt',{session:false}),  create)
+router.put('/:id',validator(schema), passport.authenticate('jwt',{session:false}), CommentIdExists, update)
+router.delete('/:id', passport.authenticate('jwt',{session:false}),CommentIdExists, destroy )
 
 // passport.authenticate('jwt',{session:false}),
 module.exports = router;
