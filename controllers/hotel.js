bdeed8c57d058330765ea831d258af48b4f69642
Nico ojeda <nico_ojeda86@hotmail.com>
2023-07-22 22:22:24 -0300

    se corrigen controladores city y hotels para buscar por nombre

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -24,7 +24,7 @@ const controller = {
         
         try{
             if(req.query.name){
-                query = {name:  {  $regex :  req.query.name, $options: 'i' + req.query.name  }}
+                query = {name:  {  $regex :  req.query.name, $options: 'i'}}
             }
             if(req.query.userId){
                 query =  
